fc451cd4da79b1083e84577d3489f115e0eeca04
VictorGoing <wiktor.idziak97@gmail.com>
2022-09-05 18:26:20 +0200

    Connect dish creator buttons and plain text to the logic

--- app/src/main/java/com/personal/shopleast/ui/dishcreator/DishCreatorFragment.java
+++ app/src/main/java/com/personal/shopleast/ui/dishcreator/DishCreatorFragment.java
@@ -1,9 +1,15 @@
 package com.personal.shopleast.ui.dishcreator;
 
 import android.os.Bundle;
+import android.text.Annotation;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +20,7 @@ import com.personal.shopleast.databinding.FragmentDishcreatorBinding;
 public class DishCreatorFragment extends Fragment {
 
     private FragmentDishcreatorBinding binding;
+    private String dishFinalName;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +30,38 @@ public class DishCreatorFragment extends Fragment {
         binding = FragmentDishcreatorBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final Button btnSaveDish = binding.btnDishSave;
+        btnSaveDish.setOnClickListener(view -> {
+            System.out.println("Dish $" + dishFinalName + "$ was saved ☺" );
+            // save dish
+        });
+
+        final Button btnAddProduct = binding.btnAddProduct;
+        btnAddProduct.setOnClickListener(view -> {
+            System.out.println("Work in progress..." );
+            // add product
+        });
+
+        final EditText dishName= binding.etxtDishName;
+        dishName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                dishFinalName = dishName.getText().toString();
+            }
+        });
+
+//        final TextView textView = binding.textDishcreator;
+//        dishCreatorViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
